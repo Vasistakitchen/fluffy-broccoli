7b56938a97c4730f3e95c75e9069c0cfe24dfdcd
Meghashyam1218 <gurrammeghashyam@outlook.com>
2023-07-27 20:13:06 +0530

    homePage components

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Hero from "../lib/components/hero.svelte";
+    import Features from "../lib/components/features.svelte";
+    import Testimonials from "../lib/components/testimonials.svelte";
+    import Footer from "../lib/components/footer.svelte";
+</script>
+
+<Hero/>
+<Features/>
+<Testimonials/>
+<Footer/>
+
