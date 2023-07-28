9fa97ba5fc9e43575c8574f7cefd15a93a262c96
NAVEEN APPANA <appananaveen19@gmail.com>
2023-07-29 01:24:41 +0530

    testi

---
+++ src/lib/components/testimonials.svelte
@@ -0,0 +1,63 @@
+<section class="testimonials bg-white">
+	<h1 class="mt-3 text-center font-bold">What our customer says</h1>
+	<div class="flex md:flex-row flex-col justify-evenly items-center my-10 gap-5">
+		<div class="w-[250px] md:w-[28%] hover:bg-zinc-100 shadow-2xl rounded-2xl p-3">
+			<img
+				class=" md:h-[50px]  h-[20px]"
+				src="https://img.icons8.com/ios-filled/50/quote-left.png"
+				alt="quote-left"
+			/>
+			<p class=" my-2 text-xs md:text-base ">
+				hjghfghvghvgbghvgvjhbhjbn njjnkjbkjbhjbghb tgvtyfvgvbbbjbhghgvh jbbjjjhjbbjnnnbbhbnnb bbh
+			</p>
+			<p class="font-bold mb-1">Naveen navy</p>
+			<div class="flex flex-row gap-2 text-yellow-500">
+				{#each Array(5) as _,index (index) }
+			<svg xmlns="http://www.w3.org/2000/svg" fill="currentcolor" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+				<path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+			  </svg>
+
+			{/each}
+			</div>
+		</div>
+		<div class="w-[250px] md:w-[28%] shadow-2xl hover:bg-zinc-100 rounded-2xl p-3">
+			<img
+				class=" md:h-[50px]  h-[20px]"
+				src="https://img.icons8.com/ios-filled/50/quote-left.png"
+				alt="quote-left"
+			/>
+			<p class=" my-2 text-xs md:text-base ">
+				hjghfghvghvgbghvgvjhbhjbn njjnkjbkjbhjbghb tgvtyfvgvbbbjbhghgvh jbbjjjhjbbjnnnbbhbnnb bbh
+			</p>
+			<p class="font-bold mb-1">Naveen navy</p>
+			<div class="flex flex-row gap-2 text-yellow-500">
+				{#each Array(5) as _,index (index) }
+			<svg xmlns="http://www.w3.org/2000/svg" fill="currentcolor" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+				<path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+			  </svg>
+
+			{/each}
+			</div>
+		</div>
+		<div class="w-[250px] md:w-[28%] shadow-2xl hover:bg-zinc-100 rounded-2xl p-3">
+			<img
+				class=" md:h-[50px]  h-[20px]"
+				src="https://img.icons8.com/ios-filled/50/quote-left.png"
+				alt="quote-left"
+			/>
+			<p class=" my-2 text-xs md:text-base ">
+				hjghfghvghvgbghvgvjhbhjbn njjnkjbkjbhjbghb tgvtyfvgvbbbjbhghgvh jbbjjjhjbbjnnnbbhbnnb bbh
+			</p>
+			<p class="font-bold mb-1">Naveen navy</p>
+			<div class="flex flex-row gap-2 text-yellow-500">
+				{#each Array(5) as _,index (index) }
+			<svg xmlns="http://www.w3.org/2000/svg" fill="currentcolor" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+				<path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+			  </svg>
+
+			{/each}
+			</div>
+		</div>
+		
+	</div>
+</section>
