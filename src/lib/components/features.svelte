fbb339ed05c057abbaa607d0dc5ad2105382c749
liferacer963 <ravikanththecool@gmail.com>
2023-07-30 19:46:08 +0530

    features

---
+++ src/lib/components/features.svelte
@@ -0,0 +1,61 @@
+<section class=" max-w-[1600px] mx-auto">
+	<div class="  ">
+		<div
+			class=" grid sm:grid-cols-2 md:grid-cols-3 p-8 justify-center gap-4 align-middle items-center text-center"
+		>
+			<div class="   rounded-lg p-4 flex flex-col gap-2 transition duration-500 hover:scale-[1.04]">
+				<p class="text-3xl sm:text-5xl text-center font-semibold">What we Offer</p>
+			</div>
+			<div
+				class="  rounded-lg p-4 flex flex-col gap-2 transform transition duration-500 hover:scale-[1.04]"
+			>
+				<img class=" justify-center rounded-lg" src="menu.webp" alt="" />
+				<p class=" text-left font-bold mt-4">MENU</p>
+				<p class="  font-normal text-justify text-sm">
+					There is no better reward for hard work than good food. Food is always more satisfying
+					after a day’s hard work
+				</p>
+			</div>
+			<div
+				class="  rounded-lg p-4 flex flex-col gap-2 transform transition duration-500 hover:scale-[1.04]"
+			>
+				<img class=" justify-center rounded-lg" src="menu.webp" alt="" />
+				<p class=" text-left font-bold mt-4">OPEN DINING</p>
+				<p class="  font-normal text-justify text-sm">
+					There is no better reward for hard work than good food. Food is always more satisfying
+					after a day’s hard work
+				</p>
+			</div>
+			<div
+				class="  rounded-lg p-4 flex flex-col gap-2 transform transition duration-500 hover:scale-[1.04]"
+			>
+				<img class=" justify-center rounded-lg" src="menu.webp" alt="" />
+				<p class=" text-left font-bold mt-4">DOOR STEP DELIVERY</p>
+				<p class="  font-normal text-justify text-sm">
+					There is no better reward for hard work than good food. Food is always more satisfying
+					after a day’s hard work
+				</p>
+			</div>
+			<div
+				class="  rounded-lg p-4 flex flex-col gap-2 transform transition duration-500 hover:scale-[1.04]"
+			>
+				<img class=" justify-center rounded-lg" src="menu.webp" alt="" />
+				<p class=" text-left font-bold mt-4">OPEN DINING</p>
+				<p class="  font-normal text-justify text-sm">
+					There is no better reward for hard work than good food. Food is always more satisfying
+					after a day’s hard work
+				</p>
+			</div>
+			<div
+				class="  rounded-lg p-4 flex flex-col gap-2 transform transition duration-500 hover:scale-[1.04]"
+			>
+				<img class=" justify-center rounded-lg" src="menu.webp" alt="" />
+				<p class=" text-left font-bold mt-4">OPEN DINING</p>
+				<p class="  font-normal text-justify text-sm">
+					There is no better reward for hard work than good food. Food is always more satisfying
+					after a day’s hard work
+				</p>
+			</div>
+		</div>
+	</div>
+</section>
